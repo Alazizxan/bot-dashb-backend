6bb98376fb03175b9cbbd73baa3e6aa9106a13c9
Pulatov Azizkhan <azikcanpapagul@gmail.com>
2026-02-11 14:10:22 +0500

    jwt probleme

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -2,9 +2,17 @@ import jwt from "jsonwebtoken";
 import { config } from "../config";
 
 export function signJwt(payload: object) {
-  return jwt.sign(payload, config.jwtSecret, { expiresIn: config.jwtExpiresIn });
+  if (!config.jwtSecret) throw new Error("JWT_SECRET is not defined");
+
+  return jwt.sign(
+    payload,
+    config.jwtSecret,
+    { expiresIn: config.jwtExpiresIn } as unknown as jwt.SignOptions
+  );
 }
 
 export function verifyJwt(token: string) {
+  if (!config.jwtSecret) throw new Error("JWT_SECRET is not defined");
+
   return jwt.verify(token, config.jwtSecret) as any;
 }
